091f0123d3db8a66483a6c31ccf1d88d8ff58584
Max Borzenko <zzzking999@yandex.ru>
2022-12-04 15:20:46 +0700

    use request param instead of path variable according to contract

--- order/src/main/java/com/brzzznko/apigateway/order/controller/OrderController.java
+++ order/src/main/java/com/brzzznko/apigateway/order/controller/OrderController.java
@@ -4,7 +4,7 @@ import com.brzzznko.apigateway.order.model.OrderDTO;
 import com.brzzznko.apigateway.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +15,8 @@ public class OrderController {
 
     private final OrderService service;
 
-    @GetMapping("api/v1/orders/{client_id}")
-    public List<OrderDTO> getOrders(@PathVariable("client_id") Long id) {
+    @GetMapping("api/v1/orders/")
+    public List<OrderDTO> getOrders(@RequestParam("client_id") Long id) {
         return service.getOrders(id);
     }
 
